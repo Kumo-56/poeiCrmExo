9ae00d7ab1ce53f3d8d48a063f5ed5b9ecf03f3b
Benjamin L <kumo-56@hotmail.fr>
2022-05-23 12:06:40 +0200

    plan

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LayoutModule} from "../layout/layout.module";
+import {IconsModule} from "../icons/icons.module";
+import {TemplateModule} from "../template/template.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    LayoutModule, IconsModule, TemplateModule
+  ]
+})
+export class CoreModule { }
