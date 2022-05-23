a1a9a550bc395b0522de69640f8144c8457c9dee
Benjamin L <kumo-56@hotmail.fr>
2022-05-23 15:46:53 +0200

    layout2

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,18 +3,37 @@ import { CommonModule } from '@angular/common';
 import {LayoutModule} from "../layout/layout.module";
 import {IconsModule} from "../icons/icons.module";
 import {TemplateModule} from "../template/template.module";
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { Nav2Component } from './components/nav2/nav2.component';
+import { Header2Component } from './components/header2/header2.component';
+import { Footer2Component } from './components/footer2/footer2.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    Nav2Component,
+    Header2Component,
+    Footer2Component
+  ],
   imports: [
     CommonModule
   ],
   exports:[
     LayoutModule,
     IconsModule,
-    TemplateModule
+    TemplateModule,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    Nav2Component,
+    Header2Component,
+    Footer2Component
   ]
 })
 export class CoreModule { }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LayoutComponent } from './components/layout/layout.component';
+import { Layout2Component } from './components/layout2/layout2.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    Layout2Component
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    LayoutComponent,
+    Layout2Component
   ]
 })
 export class LayoutModule { }
